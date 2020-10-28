7097b6bb8c56604fd5cd375806a9f86e44a01273
Yongfeng LI <wliyongfeng@gmail.com>
2020-10-28 17:43:53 +0800

    Add github to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,28 +1,69 @@
 import styled from "styled-components";
 import React from "react";
 import { ReactComponent as Generator } from './open-generator.svg'
+import { ReactComponent as Github } from "./github.svg";
 
 const Footer = styled.footer`
   height: 60px;
   background: #1D253C;
   
   display: flex;
+  justify-content: space-between;
   align-items: center;
   padding: 0 60px;
   
-  & > span {
-    color: rgba(255, 255, 255, 0.72);
-    font-size: 14px;
-    line-height: 24px;
-    margin-left: 50px;
+  div.meta {
+    display: flex;
+    align-items: center;
+    & > span {
+      color: rgba(255, 255, 255, 0.72);
+      font-size: 14px;
+      line-height: 24px;
+      margin-left: 50px;
+    }
+  }
+  
+  ul, li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  li {
+    opacity: 0.72;
+    &:hover {
+      opacity: 1;
+    }
+  }
+  
+  ul {
+    display: inline-flex;
+    & > li {
+      padding: 0 16px;
+    }
   }
 `;
 
 export default function PageFooter() {
   return (
     <Footer>
-      <Generator />
-      <span>Version v0.1</span>
+      <div className="meta">
+        <Generator />
+        <span>Version v0.1</span>
+      </div>
+      <div className="contacts">
+        <ul>
+          <li>
+            <a
+              href="https://github.com/opensquare-network/open-generator"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Github width="20" height="20" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </Footer>
   )
 }
