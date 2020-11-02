d5e88153e77caad6ea5f3d82a39991b23db9d554
Yongfeng LI <wliyongfeng@gmail.com>
2020-11-02 15:48:53 +0800

    V1

--- README.md
+++
@@ -1,3 +0,0 @@
-# Open Generator
-
-Open generator is a tool to help people without design skills to generate well formatted knowledge tip image to broadcast.
--- src/components/Preview/Card.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const TipCard = styled.section`
-  background: #FFFFFF;
-  
-  box-shadow: 0px 4px 24px rgba(29, 37, 60, 0.04);
-`;
-
-export default TipCard
