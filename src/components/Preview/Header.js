7b93f458314a357a45d0fb93d85a77f132a123e1
Yongfeng LI <wliyongfeng@gmail.com>
2020-11-04 08:35:55 +0800

    Reduce header line height

--- src/components/Preview/Header.js
+++ src/components/Preview/Header.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Header = styled.header`
   font-weight: bold;
   font-size: 18px;
-  line-height: 36px;
+  line-height: 26px;
   
   margin-bottom: 8px;
 `;
